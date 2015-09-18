6b3928005fe76fa0d4c333a1cccebdc3284dda8c
s2kw <slownin9232@gmail.com>
2015-09-18 21:53:43 +0900

    Add CameraDolly script.

---
+++ jigaX/Scripts/Camera/CameraDolly.cs
@@ -0,0 +1,48 @@
+﻿/*-------------------------------------------------
+
+	System Designed,
+	Code Written,
+	by Kunihiro Sasakawa as s2kw@jigax.jp
+
+
+CameraDorry.cs
+
+Date:
+Description:
+
+-------------------------------------------------*/
+
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using UniRx;
+
+namespace jigaX{
+public class CameraDolly : MonoBehaviour {
+
+	public Transform followingTarget;
+
+	// Use this for initialization
+	void Start () {
+		if( this.followingTarget == null ){
+			Debug.LogError("following target is null",this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( this.followingTarget == null ) return;
+		
+		this.transform.position = followingTarget.position;
+	}
+	public void Apply(){
+		this.transform.position = followingTarget.position;
+	}
+	
+	
+	
+	
+}
+}
---
+++ jigaX/Scripts/Editor/CameraDollyInspector.cs
@@ -0,0 +1,39 @@
+﻿/*-------------------------------------------------
+
+	System Designed,
+	Code Written,
+	by Kunihiro Sasakawa as s2kw@jigax.jp
+
+
+CameraDorryInspector.cs
+
+Date:
+Description:
+
+-------------------------------------------------*/
+
+
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using jigaX;
+//using UniRx;
+
+[CustomEditor(typeof(CameraDolly))]
+public class CameraDollyInspector : Editor {
+	
+	CameraDolly script;
+
+	bool first = true;
+	
+	public override void OnInspectorGUI(){
+		script = script ?? target as CameraDolly;
+		
+		if( GUILayout.Button( "Apply script value to Transform" ) ){
+			script.Apply();
+		}
+		DrawDefaultInspector();
+	}	
+}
