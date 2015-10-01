408ab6939e707d24813dfb8513fc9e633a7627a1
s2kw <slownin9232@gmail.com>
2015-10-01 20:08:40 +0900

    Added Gauge UI component.

---
+++ jigaX/Scripts/UI/UIGaugeController.cs
@@ -0,0 +1,31 @@
+﻿/*-------------------------------------------------
+
+	System Designed,
+	Code Written,
+	by Kunihiro Sasakawa as s2kw@jigax.jp
+
+
+UIGaugeController.cs
+
+Date:
+Description:
+
+-------------------------------------------------*/
+
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using UniRx;
+namespace jigaX{
+
+public class UIGaugeController : MonoBehaviour {
+
+	[SerializeField] UnityEngine.UI.Image bg;
+	[SerializeField] UnityEngine.UI.Image tmp;
+	[SerializeField] UnityEngine.UI.Image fill;
+	[SerializeField] UnityEngine.UI.Image frame;
+
+}
+}
