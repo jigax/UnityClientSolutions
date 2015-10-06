81e8a69a811c40f2803d46c24bc15e3025132307
s2kw <slownin9232@gmail.com>
2015-10-06 17:04:27 +0900

    Add interface for camera angle as Vector2.

--- jigaX/Scripts/Camera/CameraDolly.cs
+++ jigaX/Scripts/Camera/CameraDolly.cs
@@ -37,6 +37,9 @@ public class CameraDolly : MonoBehaviour {
 		
 		//this.transform.position = followingTarget.position;
 		this.Apply();
+		
+		var a = this.cameraDir;
+		
 	}
 	[SerializeField]float defaultHeight = 0f; 
 	// Inspectorのボタンで呼ばせる関数
@@ -48,8 +51,28 @@ public class CameraDolly : MonoBehaviour {
 	
 	# endregion //Trigger from Inspectors
 	
-	
-	
-	
+	Camera m_mainCam;
+	Camera mainCam{
+		get{
+			this.m_mainCam = Camera.main;
+			return this.m_mainCam;
+		}
+		set{
+			this.m_mainCam = value;
+		}
+	}
+	public Vector2 cameraDir{
+		get{
+			var p = this.transform.position;
+			var dollyPos = new Vector2( p.x, p.z );
+			var mcamP = this.mainCam.transform.position;
+			var maincamPos = new Vector2( mcamP.x, mcamP.z );
+			var a = ( dollyPos - maincamPos ).normalized;
+			this.camDir = a;
+			return a;
+		}
+	}
+	[SerializeField] Vector2 camDir;
+
 }
 }
