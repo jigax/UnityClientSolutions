3024e6017b590908aa4fb1634e846bd3a4a339a3
s2kw <slownin9232@gmail.com>
2015-10-28 17:54:05 +0900

    Add movie player and Assetbundle sample code.

---
+++ jigaX/Scripts/Editor/AssetBundleExporter.cs
@@ -0,0 +1,34 @@
+﻿/*-------------------------------------------------
+
+	System Designed,
+	Code Written,
+	by Kunihiro Sasakawa as s2kw@jigax.jp
+
+
+AssetBundleExporter.cs
+
+Date:
+Description:
+
+-------------------------------------------------*/
+
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using UniRx;
+# if UNITY_EDITOR
+using UnityEditor;
+public class CreateAssetBundles
+{
+    [MenuItem ("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles ()
+    {
+        BuildPipeline.BuildAssetBundles ("AssetBundles");
+    }
+}
+
+# endif
+
+// } // namespace
---
+++ jigaX/Scripts/UI/MovieStreamPlayer.cs
@@ -0,0 +1,115 @@
+﻿/*-------------------------------------------------
+
+	System Designed,
+	Code Written,
+	by Kunihiro Sasakawa as s2kw@jigax.jp
+
+
+MovieStreamPlayer.cs
+
+Date:
+Description:
+
+-------------------------------------------------*/
+
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//using UniRx;
+# if UNITY_EDITOR
+using UnityEditor;
+[CustomEditor( typeof( MovieStreamPlayer ) )]
+public class MovieStreamPlayerInspector : Editor{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		var script = target as MovieStreamPlayer;
+		
+		if( GUILayout.Button("Load movie as OGV") ){
+			script.LoadOGVFile();
+		}
+		if( GUILayout.Button("Play movie as OGV") ){
+			script.PlayOGVFile();
+		}
+		
+		if( GUILayout.Button("Play movie as OGV") ){
+			script.PlayOGVFile();
+		}
+	}
+}
+# endif
+
+// namespace jigaX{
+public class MovieStreamPlayer : MonoBehaviour {
+	const string oggMovieSampleURL = "http://techslides.com/demos/sample-videos/small.ogv";
+
+	[SerializeField]Renderer movieScreen;
+	WWW www;
+	void Awake(){
+		
+	}
+	
+	public void LoadOGVFile(){
+		/// プロジェクトフォルダのAssetsフォルダと同じ階層にAssetBundlesフォルダがある想定
+		this.www = new WWW("file://" + Application.dataPath +"/../AssetBundles/B.oggtheora.ogv");
+		this.movieScreen.material.SetTexture( "_MainTex",this.www.movie as MovieTexture );
+	}
+	public void PlayOGVFile(){
+		StartCoroutine( this._PlayOGVFile() );
+	}
+	IEnumerator _PlayOGVFile(){
+		
+	    MovieTexture m = movieScreen.material.GetTexture("_MainTex") as MovieTexture;
+		
+		while( ! m.isReadyToPlay ){
+			yield return 0;
+		}
+
+        if (!m.isPlaying){
+            m.Play();
+		}
+	}
+
+	public void PlayAssetBundleFile(){
+		StartCoroutine( this.LoadAndPlayFromAssetBundle() );
+	}
+
+	// Assetbundleを読み込んで再生をする
+	IEnumerator LoadAndPlayFromAssetBundle () {
+		this.isReady = false;
+		// load local assetbundle
+		WWW wwwData = new WWW( "file://" + Application.dataPath +"/../AssetBundles/test.ttt");		
+		
+		while( ! wwwData.isDone )
+			yield return 0;
+
+		AssetBundle asset = wwwData.assetBundle;
+
+		// movie の読み込み
+		AssetBundleRequest req = asset.LoadAssetAsync("domo", typeof(MovieTexture));
+
+		yield return req;
+		
+        movieScreen.material.SetTexture( "_MainTex", req.asset as MovieTexture );
+		
+		StartCoroutine( this._PlayOGVFile() );		
+		
+		// 引数で、全てunloadかバンドルから圧縮されたデータのみunloadするか決める
+		asset.Unload( false );
+		
+		/// wwwを破棄
+		wwwData.Dispose();
+		
+		Debug.Log( "Application.dataPath:" + Application.dataPath );
+		Debug.Log( "Application.persistentDataPath:" + Application.persistentDataPath );
+		
+	}
+	[SerializeField] bool isReady = false;
+	// Update is called once per frame
+	void Update () {
+	}
+}
+
+// } // namespace
